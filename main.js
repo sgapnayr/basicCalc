d96675e9bfefa750c17d1a4e4c016d486a01ed1f
“Ryan” <pagsmanagement@gmail.com>
2022-05-04 11:25:35 -0500

    pushcss

--- main.js
+++ main.js
@@ -1,72 +1,46 @@
-/* Display */
-let display = document.getElementById('sum-div')
+// /* Display */
+// let display = document.getElementById('sum-div')
 
-/* Operators */
-const add = document.getElementById('add')
-const sub = document.getElementById('sub')
-const mult = document.getElementById('mult')
-const div = document.getElementById('div')
+// /* Operators */
+// const add = document.getElementById('add')
+// const sub = document.getElementById('sub')
+// const mult = document.getElementById('mult')
+// const div = document.getElementById('div')
 
-/* Inputs */
-const inputs = document.querySelectorAll('.input')
-const buttons = document.querySelectorAll('.button')
-const num1 = document.getElementById('num1')
-const num2 = document.getElementById('num2')
+// /* Inputs */
+// const inputs = document.querySelectorAll('.input')
+// const buttons = document.querySelectorAll('.button')
+// const num1 = document.getElementById('num1')
+// const num2 = document.getElementById('num2')
 
-/* Functionality */
-add.addEventListener('click', () => {
-    output = parseInt(num1.value) + parseInt(num2.value)
-    display.innerText = output
-})
+// /* Functionality */
+// add.addEventListener('click', () => {
+//     output = parseInt(num1.value) + parseInt(num2.value)
+//     display.innerText = output
+// })
 
-sub.addEventListener('click', () => {
-    output = parseInt(num1.value) - parseInt(num2.value)
-    display.innerText = output
-})
+// sub.addEventListener('click', () => {
+//     output = parseInt(num1.value) - parseInt(num2.value)
+//     display.innerText = output
+// })
 
-mult.addEventListener('click', () => {
-    output = parseInt(num1.value) * parseInt(num2.value)
-    display.innerText = output
-})
+// mult.addEventListener('click', () => {
+//     output = parseInt(num1.value) * parseInt(num2.value)
+//     display.innerText = output
+// })
 
-div.addEventListener('click', () => {
-    output = parseInt(num1.value) / parseInt(num2.value)
-    display.innerText = output
-})
+// div.addEventListener('click', () => {
+//     output = parseInt(num1.value) / parseInt(num2.value)
+//     display.innerText = output
+// })
 
-/* Error Handling */
-inputs.forEach((input) => {
-    input.addEventListener('keyup', (event) => {
-        if (isNaN(input.value)) {
-            input.classList.add('red')
-        } else {
-            input.classList.remove('red')
-        }
-    })
-})
-
-
-// buttons.forEach((button) => {
-//     button.addEventListener('click', (e) => {
-//         let number1 = parseInt(num1.value)
-//         let number2 = parseInt(num2.value)
-//         switch (operator) {
-//             case '+':
-//                 let op = number1 + number2
-//                 display.innerText = op
-//                 break;
-//             case '-':
-//                 let op = number1 - number2
-//                 display.innerText = op
-//                 break;
-//             case '*':
-//                 let op = number1 * number2
-//                 display.innerText = op
-//                 break;
-//             case '/':
-//                 let op = number1 / number2
-//                 display.innerText = op
-//                 break;
+// /* Error Handling */
+// inputs.forEach((input) => {
+//     input.addEventListener('keyup', (event) => {
+//         if (isNaN(input.value)) {
+//             input.classList.add('red')
+//         } else {
+//             input.classList.remove('red')
 //         }
 //     })
 // })
