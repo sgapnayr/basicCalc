74ca79e82dad5af71cf17c75fea47d7b1ab3d54d
“Ryan” <pagsmanagement@gmail.com>
2022-05-04 12:03:41 -0500

    switch case

--- main.js
+++ main.js
@@ -28,44 +28,25 @@ inputs.forEach((input) => {
 })
 
 /* Functionality */
-add.addEventListener('click', () => {
-    output = parseInt(num1.value) + parseInt(num2.value)
-    display.innerText = output
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let number1 = parseInt(num1.value)
+        let number2 = parseInt(num2.value)
+        let output;
+        switch (e.target.innerText) {
+            case '+':
+                output = number1 + number2
+                break
+            case '-':
+                output = number1 - number2
+                break
+            case 'X':
+                output = number1 * number2
+                break
+            case '/':
+                output = (number1 / number2).toFixed(3)
+                break
+        }
+        display.innerText = output
+    })
 })
-
-sub.addEventListener('click', () => {
-    output = parseInt(num1.value) - parseInt(num2.value)
-    display.innerText = output
-})
-
-mult.addEventListener('click', () => {
-    output = parseInt(num1.value) * parseInt(num2.value)
-    display.innerText = output
-})
-
-div.addEventListener('click', () => {
-    output = parseInt(num1.value) / parseInt(num2.value)
-    display.innerText = output
-})
-
-// calculate = (event) => {
-//     let num1 = parseInt(input1.value);
-//     let num2 = parseInt(input2.value);
-//     if (isNaN(num1) || isNaN(num2)) {
-//       outputField.innerText = "Numbers please!";
-//       outputField.classList.add("red");
-//       return;
-//     }
-//     let output;
-//     switch (event.target.innerText) {
-//       case "+":
-//         output = num1 + num2;
-//       case "-":
-//         output = num1 - num2;
-//       case "x":
-//         output = num1 * num2;
-//       default:
-//         output = num1 / num2;
-//     }
-//     refreshOutput(output);
-//   };
