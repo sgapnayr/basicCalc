a69d122a131e31c33139f80afdeea59f5878e445
“Ryan” <pagsmanagement@gmail.com>
2022-05-04 11:48:12 -0500

    error handling

--- main.js
+++ main.js
@@ -1,46 +1,71 @@
-// /* Display */
-// let display = document.getElementById('sum-div')
+/* Display */
+let display = document.getElementById('sum-div')
+let error = document.getElementById('error')
 
-// /* Operators */
-// const add = document.getElementById('add')
-// const sub = document.getElementById('sub')
-// const mult = document.getElementById('mult')
-// const div = document.getElementById('div')
+/* Operators */
+const add = document.getElementById('add')
+const sub = document.getElementById('sub')
+const mult = document.getElementById('mult')
+const div = document.getElementById('div')
 
-// /* Inputs */
-// const inputs = document.querySelectorAll('.input')
-// const buttons = document.querySelectorAll('.button')
-// const num1 = document.getElementById('num1')
-// const num2 = document.getElementById('num2')
+/* Inputs */
+const inputs = document.querySelectorAll('.input')
+const buttons = document.querySelectorAll('.button')
+const num1 = document.getElementById('num1')
+const num2 = document.getElementById('num2')
 
-// /* Functionality */
-// add.addEventListener('click', () => {
-//     output = parseInt(num1.value) + parseInt(num2.value)
-//     display.innerText = output
-// })
+/* Error Handling */
+inputs.forEach((input) => {
+    input.addEventListener('keyup', () => {
+        if (isNaN(input.value)) {
+            input.classList.add('red')
+            error.innerText = "Please Enter a Number"
+        } else {
+            input.classList.remove('red')
+            error.innerText = ""
+        }
+    })
+})
 
-// sub.addEventListener('click', () => {
-//     output = parseInt(num1.value) - parseInt(num2.value)
-//     display.innerText = output
-// })
+/* Functionality */
+add.addEventListener('click', () => {
+    output = parseInt(num1.value) + parseInt(num2.value)
+    display.innerText = output
+})
 
-// mult.addEventListener('click', () => {
-//     output = parseInt(num1.value) * parseInt(num2.value)
-//     display.innerText = output
-// })
+sub.addEventListener('click', () => {
+    output = parseInt(num1.value) - parseInt(num2.value)
+    display.innerText = output
+})
 
-// div.addEventListener('click', () => {
-//     output = parseInt(num1.value) / parseInt(num2.value)
-//     display.innerText = output
-// })
+mult.addEventListener('click', () => {
+    output = parseInt(num1.value) * parseInt(num2.value)
+    display.innerText = output
+})
 
-// /* Error Handling */
-// inputs.forEach((input) => {
-//     input.addEventListener('keyup', (event) => {
-//         if (isNaN(input.value)) {
-//             input.classList.add('red')
-//         } else {
-//             input.classList.remove('red')
-//         }
-//     })
-// })
+div.addEventListener('click', () => {
+    output = parseInt(num1.value) / parseInt(num2.value)
+    display.innerText = output
+})
+
+// calculate = (event) => {
+//     let num1 = parseInt(input1.value);
+//     let num2 = parseInt(input2.value);
+//     if (isNaN(num1) || isNaN(num2)) {
+//       outputField.innerText = "Numbers please!";
+//       outputField.classList.add("red");
+//       return;
+//     }
+//     let output;
+//     switch (event.target.innerText) {
+//       case "+":
+//         output = num1 + num2;
+//       case "-":
+//         output = num1 - num2;
+//       case "x":
+//         output = num1 * num2;
+//       default:
+//         output = num1 / num2;
+//     }
+//     refreshOutput(output);
+//   };
